fa5b92524d2d85795bda9f65929ab7fb8c46a1a8
Koutyboy <victor.karlsson.github@gmail.com>
2017-09-05 10:10:24 +0200

    Initial commit

---
+++ OnionPatcher/Patcher.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace OnionPatcher
+{
+    partial class Patcher
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Patcher));
+            this.LocationGroupBox = new System.Windows.Forms.GroupBox();
+            this.StatusLabel = new System.Windows.Forms.Label();
+            this.PathBrowseButton = new System.Windows.Forms.Button();
+            this.PathBox = new System.Windows.Forms.TextBox();
+            this.PatchButton = new System.Windows.Forms.Button();
+            this.RestoreButton = new System.Windows.Forms.Button();
+            this.LocationGroupBox.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // LocationGroupBox
+            // 
+            this.LocationGroupBox.Controls.Add(this.StatusLabel);
+            this.LocationGroupBox.Controls.Add(this.PathBrowseButton);
+            this.LocationGroupBox.Controls.Add(this.PathBox);
+            this.LocationGroupBox.Location = new System.Drawing.Point(12, 12);
+            this.LocationGroupBox.Name = "LocationGroupBox";
+            this.LocationGroupBox.Size = new System.Drawing.Size(387, 76);
+            this.LocationGroupBox.TabIndex = 0;
+            this.LocationGroupBox.TabStop = false;
+            this.LocationGroupBox.Text = "ONI Root Directory";
+            // 
+            // StatusLabel
+            // 
+            this.StatusLabel.AutoSize = true;
+            this.StatusLabel.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.StatusLabel.Location = new System.Drawing.Point(87, 51);
+            this.StatusLabel.Name = "StatusLabel";
+            this.StatusLabel.Size = new System.Drawing.Size(84, 14);
+            this.StatusLabel.TabIndex = 3;
+            this.StatusLabel.Text = "Status: N/A";
+            // 
+            // PathBrowseButton
+            // 
+            this.PathBrowseButton.Location = new System.Drawing.Point(6, 47);
+            this.PathBrowseButton.Name = "PathBrowseButton";
+            this.PathBrowseButton.Size = new System.Drawing.Size(75, 23);
+            this.PathBrowseButton.TabIndex = 1;
+            this.PathBrowseButton.Text = "Browse";
+            this.PathBrowseButton.UseVisualStyleBackColor = true;
+            this.PathBrowseButton.Click += new System.EventHandler(this.PathBrowseButton_Click);
+            // 
+            // PathBox
+            // 
+            this.PathBox.Enabled = false;
+            this.PathBox.Location = new System.Drawing.Point(6, 19);
+            this.PathBox.Name = "PathBox";
+            this.PathBox.Size = new System.Drawing.Size(375, 22);
+            this.PathBox.TabIndex = 0;
+            // 
+            // PatchButton
+            // 
+            this.PatchButton.Enabled = false;
+            this.PatchButton.Location = new System.Drawing.Point(18, 145);
+            this.PatchButton.Name = "PatchButton";
+            this.PatchButton.Size = new System.Drawing.Size(75, 23);
+            this.PatchButton.TabIndex = 1;
+            this.PatchButton.Text = "Patch";
+            this.PatchButton.UseVisualStyleBackColor = true;
+            this.PatchButton.Click += new System.EventHandler(this.PatchButton_Click);
+            // 
+            // RestoreButton
+            // 
+            this.RestoreButton.Enabled = false;
+            this.RestoreButton.Location = new System.Drawing.Point(275, 145);
+            this.RestoreButton.Name = "RestoreButton";
+            this.RestoreButton.Size = new System.Drawing.Size(124, 23);
+            this.RestoreButton.TabIndex = 2;
+            this.RestoreButton.Text = "Restore Backup";
+            this.RestoreButton.UseVisualStyleBackColor = true;
+            this.RestoreButton.Click += new System.EventHandler(this.RestoreButton_Click);
+            // 
+            // Patcher
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 14F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(411, 180);
+            this.Controls.Add(this.RestoreButton);
+            this.Controls.Add(this.PatchButton);
+            this.Controls.Add(this.LocationGroupBox);
+            this.Font = new System.Drawing.Font("Monaco", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Patcher";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "OnionPatcher";
+            this.LocationGroupBox.ResumeLayout(false);
+            this.LocationGroupBox.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox LocationGroupBox;
+        private System.Windows.Forms.Button PathBrowseButton;
+        private System.Windows.Forms.TextBox PathBox;
+        private System.Windows.Forms.Button PatchButton;
+        private System.Windows.Forms.Button RestoreButton;
+        private System.Windows.Forms.Label StatusLabel;
+    }
+}
+
---
+++ OnionPatcher/Patcher.cs
@@ -0,0 +1,305 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+
+namespace OnionPatcher
+{
+    public partial class Patcher : Form
+    {
+        public Patcher()
+        {
+            InitializeComponent();
+        }
+
+        private string GetRoot()
+        {
+            return PathBox.Text;
+        }
+
+        private string GetAssemblyDir()
+        {
+            return PathBox.Text + "/OxygenNotIncluded_Data/Managed";
+        }
+
+        private void SetLastPath(FolderBrowserDialog dialog)
+        {
+            if (File.Exists("data/path.txt"))
+            {
+                dialog.SelectedPath = File.ReadAllText("data/path.txt");
+            }
+        }
+
+        private void ValidateBackup()
+        {
+            RestoreButton.Enabled = false;
+            if (File.Exists("data/Assembly-CSharp.dll"))
+            {
+                RestoreButton.Enabled = true;
+            }
+        }
+
+        private void ValidateAssembly(string path)
+        {
+            PatchButton.Enabled = false;
+            RestoreButton.Enabled = false;
+            // Check if requires assemblies exists
+            DefaultAssemblyResolver resolver    = new DefaultAssemblyResolver();
+            AssemblyDefinition CSharp           = null;
+
+            resolver.AddSearchDirectory(path);
+            try
+            {
+                CSharp = AssemblyDefinition.ReadAssembly(path + Path.DirectorySeparatorChar + "Assembly-CSharp.dll", new ReaderParameters { AssemblyResolver = resolver });
+            }
+            catch
+            {
+                StatusLabel.Text        = "Status: No Assembly in directory.";
+                StatusLabel.ForeColor   = Color.Red;
+                return;
+            }
+
+            ValidateBackup();
+
+            // Check if assemblies are already patched
+            TypeDefinition Patched = null;
+            try
+            {
+                Patched = CSharp.Modules
+                    .First(M => M.Name == "Assembly-CSharp.dll")
+                    .Types
+                    .First(T => T.Name == "OnionPatched");
+            }
+            catch
+            {
+                Patched = null;
+            }
+
+            if (Patched != null)
+            {
+                StatusLabel.Text        = "Status: Already Patched.";
+                StatusLabel.ForeColor   = Color.Red;
+                return;
+            }
+            else
+            {
+                StatusLabel.Text        = "Status: OK.";
+                StatusLabel.ForeColor   = Color.Green;
+            }
+
+            
+            PatchButton.Enabled = true;
+        }
+
+        private void PathBrowseButton_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog dialog = new FolderBrowserDialog())
+            {
+                SetLastPath(dialog);
+                DialogResult result = dialog.ShowDialog();
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(dialog.SelectedPath))
+                {
+                    File.WriteAllText("data/path.txt", dialog.SelectedPath);
+                    PathBox.Text = dialog.SelectedPath;
+                    ValidateAssembly(GetAssemblyDir());
+                }
+            }
+        }
+
+        private void RestoreButton_Click(object sender, EventArgs e)
+        {
+            File.Copy("data/Assembly-CSharp.dll", GetAssemblyDir() + Path.DirectorySeparatorChar + "Assembly-CSharp.dll", true);
+            ValidateAssembly(GetAssemblyDir());
+        }
+
+        private void PatchButton_Click(object sender, EventArgs e)
+        {
+            // Move the necessary files
+            File.Copy("data/onion.json", GetRoot() + Path.DirectorySeparatorChar + "onion.json", true);
+            File.Copy("data/OnionHooks.dll", GetAssemblyDir() + Path.DirectorySeparatorChar + "OnionHooks.dll", true);
+            File.Copy(GetAssemblyDir() + Path.DirectorySeparatorChar + "Assembly-CSharp.dll", "data/Assembly-CSharp.dll", true);
+            
+            // Patch the Assembly
+            // Check if requires assemblies exists
+            DefaultAssemblyResolver resolver    = new DefaultAssemblyResolver();
+            AssemblyDefinition CSharp           = null;
+            AssemblyDefinition Hook             = null;
+
+            resolver.AddSearchDirectory(GetAssemblyDir());
+            try
+            {
+                CSharp = AssemblyDefinition.ReadAssembly(GetAssemblyDir() + Path.DirectorySeparatorChar + "Assembly-CSharp.dll",    new ReaderParameters { AssemblyResolver = resolver });
+                Hook  = AssemblyDefinition.ReadAssembly(GetAssemblyDir() + Path.DirectorySeparatorChar + "OnionHooks.dll",         new ReaderParameters { AssemblyResolver = resolver });
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                return;
+            }
+
+            if (CSharp == null || Hook == null)
+            {
+                return;
+            }
+
+            /* Find all injection-points for Assembly-CSharp.dll */
+            MethodDefinition InitRandom = CSharp.Modules.First(M => M.Name == "Assembly-CSharp.dll")
+                .Types.First(T => T.Name == "WorldGen")
+                .Methods.First(F => F.Name == "InitRandom");
+
+
+            MethodDefinition OnInitRandom = Hook.Modules.First(M => M.Name == "OnionHooks.dll")
+                .Types.First(T => T.Name == "Hooks")
+                .Methods.First(M => M.Name == "OnInitRandom");
+
+            MethodDefinition DoWorldGen = CSharp.Modules.First(M => M.Name == "Assembly-CSharp.dll")
+                .Types.First(T => T.Name == "OfflineWorldGen")
+                .Methods.First(F => F.Name == "DoWorldGen");
+
+            MethodDefinition DebugHandlerCTOR = CSharp.Modules.First(M => M.Name == "Assembly-CSharp.dll")
+                .Types.First(T => T.Name == "DebugHandler")
+                .Methods.First(F => F.Name == ".ctor");
+
+            Console.WriteLine(DebugHandlerCTOR.Name);
+
+
+            if (InitRandom == null && OnInitRandom == null)
+            {
+                Console.WriteLine("Missing MethodDefinitions");
+                return;
+            }
+
+            DoWorldGen.Body.Variables.Add(new VariableDefinition("w", CSharp.MainModule.TypeSystem.Int32));
+            DoWorldGen.Body.Variables.Add(new VariableDefinition("h", CSharp.MainModule.TypeSystem.Int32));
+
+
+            /* Write IL */
+            ILProcessor proc = InitRandom.Body.GetILProcessor();
+            Instruction IP = InitRandom.Body.Instructions[0];
+            try
+            {
+                
+                byte slot_0 = 0;
+                byte slot_1 = 1;
+                byte slot_2 = 2;
+                byte slot_3 = 3;
+                byte slot_4 = 4;
+                
+                // Add hook to Assembly-CSharp.Klei.WorldGen.InitRandom
+                proc.InsertBefore(IP, IP = proc.Create(OpCodes.Ldarga_S, slot_1)); // Load the address, since we are passing by ref.
+                // Update the compatability
+                proc.InsertAfter(IP, IP = proc.Create(OpCodes.Ldarga_S, slot_2));
+                proc.InsertAfter(IP, IP = proc.Create(OpCodes.Ldarga_S, slot_3));
+                proc.InsertAfter(IP, IP = proc.Create(OpCodes.Ldarga_S, slot_4));
+                proc.InsertAfter // Add call to our dll
+                (
+                    IP,
+                    IP = proc.Create(OpCodes.Call, InitRandom.Module.Import(
+                        typeof(OnionHooks.Hooks).GetMethod("OnInitRandom", new[] { typeof(int).MakeByRefType(), typeof(int).MakeByRefType(), typeof(int).MakeByRefType(), typeof(int).MakeByRefType() })
+                    ))
+                );
+
+                // Add hook to <hook>
+                DoWorldGen.NoOptimization = true;
+                proc    = DoWorldGen.Body.GetILProcessor();
+                IP      = DoWorldGen.Body.Instructions[26];
+
+                proc.InsertAfter(proc.Body.Instructions[23], proc.Create(OpCodes.Stloc_2));
+                Instruction I = proc.Body.Instructions[26];
+                proc.InsertAfter(I, I = proc.Create(OpCodes.Stloc_3));
+                proc.InsertAfter(I, I = proc.Create(OpCodes.Ldloca_S, slot_2));
+                proc.InsertAfter(I, I = proc.Create(OpCodes.Ldloca_S, slot_3));
+
+                
+                proc.InsertBefore(IP, IP = proc.Create(OpCodes.Call, DoWorldGen.Module.Import(
+                    typeof(OnionHooks.Hooks).GetMethod("OnDoOfflineWorldGen", new [] {typeof(int).MakeByRefType(), typeof(int).MakeByRefType()})
+                    ))
+                );
+
+                
+                proc.InsertAfter(IP, IP = proc.Create(OpCodes.Ldloc_2));
+                proc.InsertAfter(IP, IP = proc.Create(OpCodes.Ldloc_3));
+                
+                FieldReference enabled = CSharp.Modules.First(M => M.Name == "Assembly-CSharp.dll")
+                .Types.First(T => T.Name == "DebugHandler").Fields.First(F => F.Name == "enabled");
+
+                FieldReference camera = CSharp.Modules.First(M => M.Name == "Assembly-CSharp.dll")
+                .Types.First(T => T.Name == "DebugHandler").Fields.First(F => F.Name == "FreeCameraMode");
+
+                Console.WriteLine(camera.Name);
+                // Add hook to <hook>
+                
+                proc = DebugHandlerCTOR.Body.GetILProcessor();
+                IP   = DebugHandlerCTOR.Body.Instructions.Last();
+                
+                
+                proc.InsertBefore(IP, IP = proc.Create(OpCodes.Ldarg_0));
+                proc.InsertAfter(IP, IP = proc.Create(OpCodes.Call, DebugHandlerCTOR.Module.Import(
+                    typeof(OnionHooks.Hooks).GetMethod("GetDebugEnabled")
+                )));
+                
+                
+                //proc.InsertBefore(IP, IP = proc.Create(OpCodes.Ldc_I4_1));
+                proc.InsertAfter(IP, IP = proc.Create(OpCodes.Stfld, enabled));
+                
+
+                
+                proc.InsertAfter(IP, IP = proc.Create(OpCodes.Call, DebugHandlerCTOR.Module.Import(
+                    typeof(OnionHooks.Hooks).GetMethod("GetDebugEnabled")
+                )));
+                
+
+                //proc.InsertAfter(IP, IP = proc.Create(OpCodes.Ldc_I4_1));
+                proc.InsertAfter(IP, IP = proc.Create(OpCodes.Stsfld, camera));
+
+                // Hook
+                
+                MethodDefinition CameraController = CSharp.Modules.First(M => M.Name == "Assembly-CSharp.dll")
+                       .Types.First(T => T.Name == "CameraController")
+                       .Methods.First(F => F.Name == ".ctor");
+
+                FieldReference MaxCameraDistance = CSharp.Modules.First(M => M.Name == "Assembly-CSharp.dll")
+                .Types.First(T => T.Name == "CameraController").Fields.First(F => F.Name == "maxOrthographicSize");
+
+                FieldReference MaxCameraDistance2 = CSharp.Modules.First(M => M.Name == "Assembly-CSharp.dll")
+                        .Types.First(T => T.Name == "CameraController").Fields.First(F => F.Name == "maxOrthographicSizeDebug");
+
+                proc = CameraController.Body.GetILProcessor();
+                IP   = CameraController.Body.Instructions.Last();
+
+                proc.InsertBefore(IP, IP = proc.Create(OpCodes.Ldarg_0));
+                proc.InsertAfter(IP, IP = proc.Create(OpCodes.Call, CameraController.Module.Import(
+                    typeof(OnionHooks.Hooks).GetMethod("GetMaxCameraShow")
+                )));
+                proc.InsertAfter(IP, IP = proc.Create(OpCodes.Stfld, MaxCameraDistance));
+
+                proc.InsertAfter(IP, IP = proc.Create(OpCodes.Ldarg_0));
+                proc.InsertAfter(IP, IP = proc.Create(OpCodes.Call, CameraController.Module.Import(
+                    typeof(OnionHooks.Hooks).GetMethod("GetMaxCameraShow")
+                )));
+                proc.InsertAfter(IP, IP = proc.Create(OpCodes.Stfld, MaxCameraDistance));
+                
+
+                // Add OnionID which is a class that identifies if the Assembly-CSharp.dll has been patched.
+                var OnionPatched = new TypeDefinition("", "OnionPatched", TypeAttributes.Class, CSharp.MainModule.TypeSystem.Object);
+                CSharp.MainModule.Types.Add(OnionPatched);
+
+                CSharp.Write(GetAssemblyDir() + Path.DirectorySeparatorChar + "Assembly-CSharp.dll");
+                MessageBox.Show("Patch Complete!");
+                ValidateAssembly(GetAssemblyDir());
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+    }
+}
